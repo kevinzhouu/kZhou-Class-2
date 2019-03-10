030719087d38b308b955454f18d92860e92e5f1c
kevinzhouu <kevinzhou@hunterschools.org>
2019-03-10 11:53:02 -0400

    added new files

---
+++ Lesson1/Lesson.java
@@ -0,0 +1,35 @@
+public class Lesson{
+  public static void main(String[]args){
+    //System.out.print("Hello"); /*always end with a semicolon */
+    for(int i=0;i<10;i++){
+      //System.out.println(5);
+    }
+    int y = 0;
+    while (y<10){
+      y++;
+      //System.out.println("test2");
+    }
+    for(int x=10;x<=20;x+=2){
+        System.out.println(x);
+    }
+    int a = 8;
+    while (a<=18){
+      a+=2;
+      System.out.println(a);
+    }
+  }
+}
+/* Data Types
+  Primitive
+    Int (integer)
+    double (decimals)
+    char (character) e.g. 'a'
+    boolean (true or false)
+  Object type
+    String e.g "hello" */
+/* Loops
+  For Loop
+    Beginning section-intializer
+    Middle section-condition
+    Final section-increment
+  While loop*/
